af661dc57e2ce2cca7e44fdd7a992b4fa49ffa41
Lukasz Wojcik <lukasz.wojcik.22@gmail.com>
2022-10-15 18:15:17 +0200

    Make sure to skip invalid values

--- create_dataset.py
+++ create_dataset.py
@@ -11,11 +11,11 @@ try:
         data_photo = pd.read_csv(f'BIRAFFE2-photo/{subject}-Face.csv', delimiter=';', dtype='str')
         data_procedure = pd.read_csv(f'BIRAFFE2-procedure/{subject}-Procedure.csv', delimiter=';', dtype='str')
         data_procedure = data_procedure[data_procedure['COND'].notna()]
-        data_procedure.replace('None', -1, inplace=True)
+        data_procedure.replace('None', '-1', inplace=True)
 
         data_combined = data_photo.iloc[:, 2:][data_photo['FRAME-NUMBER'].isin(['0', '105', '120', '135', '15', '150'])].fillna(-1)
-        data_combined['ANS_VALENCE'] = 0
-        data_combined['ANS_AROUSAL'] = 0
+        data_combined['ANS_VALENCE'] = '0'
+        data_combined['ANS_AROUSAL'] = '0'
 
         for index, row in data_combined.iterrows():
             for index2, row2 in data_procedure.iterrows():
@@ -30,6 +30,16 @@ try:
             data_final = data_combined
         else:
             data_final = pd.concat([data_final, data_combined])
+
+    data_final = data_final.astype('float64')
+    data_final = data_final[data_final['ANGER'] >= 0]
+    data_final = data_final[data_final['CONTEMPT'] >= 0]
+    data_final = data_final[data_final['DISGUST'] >= 0]
+    data_final = data_final[data_final['FEAR'] >= 0]
+    data_final = data_final[data_final['HAPPINESS'] >= 0]
+    data_final = data_final[data_final['NEUTRAL'] >= 0]
+    data_final = data_final[data_final['SADNESS'] >= 0]
+    data_final = data_final[data_final['SURPRISE'] >= 0]
 
     print('Writing data to file dataset.csv')
     data_final.to_csv('dataset.csv', index=False)
